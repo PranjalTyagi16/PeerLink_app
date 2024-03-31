3eba918f7e8ad5f8ec6d3e736f6221c8b22ae18b
pranjal tyagi <tyagipranjal16@gmail.com>
2024-03-31 12:46:08 +0530

    implement all the features

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -17,6 +17,7 @@ import { Textarea } from "../ui/textarea";
 //import { updateUser } from "@/lib/actions/user.actions";
 import { usePathname,useRouter } from "next/navigation";
 import { createThread } from "@/lib/actions/thread.actions";
+import { useOrganization } from "@clerk/nextjs";
 interface Props{
     user:{
         id:string;
@@ -32,7 +33,8 @@ interface Props{
 function PostThread({userId}:{userId:string}){
   const router=useRouter();
   const pathname=usePathname();
-
+  
+  const {organization}=useOrganization();
 
   const form=useForm({
       resolver:zodResolver(ThreadValidation),
@@ -46,7 +48,7 @@ function PostThread({userId}:{userId:string}){
      await createThread({
         text: values.thread,
         author:userId,
-        communityId:null,
+        communityId:organization?organization.id:null,
         path:pathname,
     });
     router.push("/");
